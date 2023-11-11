fa3e5ac16a024cd3ef3a8f039a2238579a014080
shahbaz4783 <shahbaz4783@gmail.com>
2023-11-11 22:16:54 +0530

    ejs tags

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ import express from 'express';
 const app = express()
 const port = 4000
 
+const day = new Date().getDay();
+let dayType = 'weekday';
+let task = 'work hard';
+
+
+const isWeekend = (day === 0) | (day === 6);
+dayType = isWeekend && 'weekend';
+task = isWeekend && 'chill';
+
 app.get('/', (req, res) => {
-    res.send('<h1>This is EJS</h1>')
-})
+	res.render('index.ejs', { day: dayType, task: task });
+});
+
+app.get('/time', (req, res) => {
+    const data = {
+			title: 'EJS Tags',
+			second: new Date().getSeconds(),
+			item: ['apple', 'mango', 'banana'],
+		};
+	res.render('time.ejs', data);
+});
+
 
 app.listen(port, () => {
-    `Listning on Port ${port}`
-})
+	console.log(`Listning on Port ${port}`);
+});
