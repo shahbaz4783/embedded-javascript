668a9cacc9571eddefe60a2ba08347fd1b0f7b83
shahbaz4783 <shahbaz4783@gmail.com>
2023-11-12 13:44:05 +0530

    passing data to ejs

--- app.js
+++ app.js
@@ -11,7 +11,19 @@ let task = 'work hard';
 const isWeekend = (day === 0) | (day === 6);
 dayType = isWeekend && 'weekend';
 task = isWeekend && 'chill';
+let totalLetters;
 
+app.use(express.urlencoded({ extended: false }));
+
+// const countLetters = (req, res, next) => {
+//     console.log(req.body);
+// 	totalLetters = (req.body['fname'] + req.body['lname']).length;
+//     next();
+// };
+
+// app.use(countLetters)
+
+// First Challenge
 app.get('/', (req, res) => {
 	res.render('index.ejs', { day: dayType, task: task });
 });
@@ -25,6 +37,16 @@ app.get('/time', (req, res) => {
 	res.render('time.ejs', data);
 });
 
+
+// Sencond Challenge
+app.get('/count', (req, res) => {
+	res.render('namecount.ejs');
+});
+
+app.post('/count', (req, res) => {
+    const totalLetters = (req.body['fname'] + req.body['lname']).length;
+	res.render('namecount.ejs', { count: totalLetters });
+});
 
 app.listen(port, () => {
 	console.log(`Listning on Port ${port}`);
